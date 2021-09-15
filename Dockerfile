a3289f62e8d40f42b548693fa0e6049de7ff66f2
campalite <ysfturker97@gmail.com>
2021-09-15 10:39:24 +0300

    init heroku with docker

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,19 @@
-FROM tensorflow/tensorflow:2.2.0
-RUN mkdir -p /rasa_app
-WORKDIR /rasa_app
-COPY . /rasa_app
-RUN python -m pip install -U pip
-RUN pip3 install -r requirements.txt
-#RUN pip3 install --user rasa-nlu==0.14.0
-#RUN pip3 install --user rasa-core==0.13.2
-RUN python -m spacy download en
-RUN pip3 install --user rasa==2.1.2
-RUN pip3 install --user sanic==19.9.0
+#FROM tensorflow/tensorflow:2.2.0
+#RUN mkdir -p /rasa_app
+#WORKDIR /rasa_app
+#COPY . /rasa_app
+#RUN python -m pip install -U pip
+#RUN pip3 install -r requirements.txt
+##RUN pip3 install --user rasa-nlu==0.14.0
+##RUN pip3 install --user rasa-core==0.13.2
+#RUN python -m spacy download en
+#RUN pip3 install --user rasa==2.1.2
+#RUN pip3 install --user sanic==19.9.0
+
+
+FROM ubuntu:18.04
+ENTRYPOINT []
+RUN apt-get update && apt-get install -y python3 python3-pip && python3 -m pip install --no-cache --upgrade pip && pip3 install --no-cache rasa==2.1.2
+ADD . /app/
+RUN chmod +x /app/start_services.sh
+CMD /app/start_services.sh
--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,79 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
 #
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import Restarted
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionStartSession(Action):
+    def name(self) -> Text:
+        return "action_start_session"
+
+    def run(
+        self,
+        dispatcher:"CollectingDispatcher",
+        tracker: Tracker,
+        domain:"DomainDict",
+    ) -> List[Dict[Text,Any]]:
+        return [SlotSet("is_logged_in",True)]
+
+class ActionCarousel(Action):
+    def name(self) -> Text:
+        return "action_carousels"
+    
+    def run(self, dispatcher, tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
+        message = {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [
+                    {
+                        "title": "Merhaba, bugün nasılsın?",
+                        "subtitle": "$10",
+                        "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqhmyBRCngkU_OKSL6gBQxCSH-cufgmZwb2w&usqp=CAU",
+                        "buttons": [ 
+                            {
+                            "title": "Muhteşem",
+                            "payload": "muhteşem",
+                            "type": "postback"
+                            },
+                            {
+                            "title": "kötü",
+                            "payload": "Kötü",
+                            "type": "postback"
+                            }
+                        ]
+                    },
+                    {
+                        "title": "Carousel 2",
+                        "subtitle": "$12",
+                        "image_url": "https://image.freepik.com/free-vector/city-illustration_23-2147514701.jpg",
+                        "buttons": [ 
+                            {
+                            "title": "Click here",
+                            "url": "https://image.freepik.com/free-vector/city-illustration_23-2147514701.jpg",
+                            "type": "web_url"
+                            }
+                        ]
+                    }
+                ]
+                }
+        }
+        dispatcher.utter_message(attachment=message)
+        return []
+
+
+class ActionRestart(Action):
+
+    def name(self) -> Text:
+        return "action_restart"
+
+    async def run(
+        self,dispatcher,tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+
+    # custom_behavior
+
+        return [Restarted()]
